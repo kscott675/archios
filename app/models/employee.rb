6bd98dee620fdb075de9fa024ff2978fb1113689
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-04-27 19:50:57 +0000

    change sample data, add default status, changed homescreeen

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,46 +1,11 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: employees
-#
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  first_name             :string
-#  last_name              :string
-#  phone_number           :string
-#  profile_picture        :string
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  role                   :string
-#  time_zone              :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  company_id             :bigint           not null
-#  manager_id             :integer
-#
-# Indexes
-#
-#  index_employees_on_company_id            (company_id)
-#  index_employees_on_email                 (email) UNIQUE
-#  index_employees_on_reset_password_token  (reset_password_token) UNIQUE
-#
-# Foreign Keys
-#
-#  fk_rails_...  (company_id => companies.id)
-#
 class Employee < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Enum for role
   enum role: { manager: "manager", employee: "employee" }
 
-  # Validations
+  after_commit :create_initial_status
+
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
   validates :first_name, presence: true
@@ -50,34 +15,31 @@ class Employee < ApplicationRecord
   validates :phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "must be 10 digits" }
   validates :time_zone, presence: true
 
-  # Associations
   belongs_to :company
   belongs_to :manager, class_name: "Employee", foreign_key: :manager_id, optional: true
   has_many :timesheet_entries, class_name: "TimesheetEntry", foreign_key: :employee_id
   has_many :messages, foreign_key: :sender_id
-  has_many :employees, class_name: "Employee", foreign_key: "manager_id"
+  has_many :subordinates, class_name: "Employee", foreign_key: "manager_id"
   has_one :status, foreign_key: :employee_id
-  has_one :tmail_subscription, class_name: "TmailSubscription", foreign_key: "employee_id"
+  has_one :tmail_subscription, foreign_key: :employee_id
 
   scope :managers, -> { where(role: roles[:manager]) }
 
   def dashboard
-    { first_name: full_name,
+    {
+      first_name: full_name,
       company_name: company.name,
-      profile_picture_url: profile_picture? ? Rails.application.routes.url_helpers.url_for(profile_picture) : nil,
-      time_zone:,
-      role:,
-      manager: manager.present? ? Rails.application.routes.url_helpers.url_for(manager.full_name) : nil,
-      current_timesheet: current_timesheet.map(&:attributes) }
+      profile_picture_url: profile_picture_url,
+      time_zone: time_zone,
+      role: role,
+      manager: manager.present? ? manager.full_name : nil,
+      current_timesheet: current_timesheet.map(&:attributes),
+    }
   end
 
   def current_timesheet
-    today = Date.today
     pp = PayPeriod.current
-
-    return {} if pp.nil?
-
-    timesheet_entries.where(pay_period_id: pp.id)
+    pp ? timesheet_entries.where(pay_period_id: pp.id) : []
   end
 
   def total_hours_worked
@@ -89,20 +51,15 @@ class Employee < ApplicationRecord
   end
 
   def clocked_in?
-    last_entry = timesheet_entries.last
-    last_entry.present? && last_entry.ended_at.nil?
+    timesheet_entries.current.exists?
   end
 
   def clock_in
-    return if clocked_in? # Don't allow clocking in if already clocked in
-    timesheet_entries.create(started_at: Time.now)
+    timesheet_entries.create(started_at: Time.now) unless clocked_in?
   end
 
-  # Method to clock out
   def clock_out
-    return unless clocked_in? # Don't allow clocking out if not clocked in
-    last_entry = timesheet_entries.last
-    last_entry.update(ended_at: Time.now)
+    last_entry.update(ended_at: Time.now) if clocked_in?
   end
 
   def last_entry
@@ -111,5 +68,11 @@ class Employee < ApplicationRecord
 
   def has_tmail?
     tmail_subscription.present?
+  end
+
+  private
+
+  def create_initial_status
+    Status.create(employee: self, text: "Available")
   end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
 desc "Fill the database tables with some sample data"
-task reset_db: :environment do
-  Request.destroy_all
-  PayPeriod.destroy_all
-  Status.destroy_all
-  TimesheetEntry.destroy_all
-  Employee.destroy_all
-  Company.destroy_all
-  TmailSubscription.destroy_all
+task sample_data: :environment do
 end
