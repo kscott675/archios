f3c2eb004510d5a8646bcd91c982746566b8d4a2
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-04-28 13:50:35 +0000

    cleaning up db migration files

--- db/migrate/20240411221715_add_current_status_to_employees_table.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddCurrentStatusToEmployeesTable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :employees, :current_status, :string
-  end
-end
--- db/migrate/20240411222442_add_default_status_to_employees.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDefaultStatusToEmployees < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :employees, :current_status, '...'
-  end
-end
