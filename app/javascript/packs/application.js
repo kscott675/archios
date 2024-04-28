7900c40cb4d910670fd64ec5eeb27d1942ced43b
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-04-28 21:53:19 +0000

    rails-ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,22 @@
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
 
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript file within this directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+
+// Import other JavaScript files using the //= require syntax. For example:
+//= require jquery
+//= require bootstrap
+
+// Import individual JavaScript files from the /app/assets/javascripts directory or any other directory within your Rails app.
+// For example:
+//= require_tree ./components
